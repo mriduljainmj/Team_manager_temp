6ac08e10004167068b537d249063ad82b5213ce0
Mridul Jain <itsme.mriduljain@gmail.com>
2021-05-07 23:35:35 +0530

    deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db')
 const app = express();
+const path = require('path')
 
 const usersRoute = require('./routes/api/users')
 const profileRoute = require('./routes/api/profile')
@@ -12,16 +13,19 @@ connectDB();
 
 app.use(express.json({extended:false}));
 
-app.get('/',(req,res,next)=>{
-    res.send('API Running');
-})
-
-
 app.use('/api/users',usersRoute)
 app.use('/api/profile',profileRoute)
 app.use('/api/posts',postsRoute)
 app.use('/api/auth',authRoute)
 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(___dirname,'client','build','index.html'))
+    })
+}
 
 const PORT  = process.env.PORT || 5000;
 
